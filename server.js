eb3732d18fec8584de22c9e793205c6241753d27
eoinfitz14 <eoin.fitzsimons14@gmail.com>
2020-02-27 16:16:16 +0000

    Getting register functionality working without DB. Almost there

--- server.js
+++ server.js
@@ -38,6 +38,23 @@ app.post('/signin', (req,res) => {
   }
 })
 
+app.post('/register', (req,res) => {
+  const { email, name, password } = req.body; // destructuring the request and assigning the values of it to constant variables
+  var lastUserIndex = database.users.length-1;
+  database.users.push({ // pushing a new user object into the user array! NB v useful
+    id: '125', // need to make dynamic
+      name: name,
+      email: email,
+      password: password,
+      entries: 0,
+      joined: new Date()
+  })
+  /*
+  Note: I think the push isn't updating the 'DB' quickly enough so it prints out the 2nd last object rather than the last
+  */
+  res.json(database.users[lastUserIndex]);
+})
+
 app.listen(3000, () => { // listen on port 3000 and after listen happens run the function i.e the second parameter
   console.log('app is running on port 3000 ')
 }); 
