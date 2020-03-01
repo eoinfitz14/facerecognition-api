b8771547e4f83e51fe0bb0141599bd10503ba5bb
eoinfitz14 <eoin.fitzsimons14@gmail.com>
2020-03-01 18:28:45 +0000

    Backend working with frontend

--- server.js
+++ server.js
@@ -1,26 +1,38 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt-nodejs'); 
+const cors = require('cors');
 
 const app = express(); // create the app
+
+//Middleware
 app.use(bodyParser.json());
+app.use(cors()); // security middleware. Google would not let us access the api through the front end without it
 
 const database = { // temporary "database"..... just an array of objects
   users: [
     {
       id: '123',
       name: 'John',
-      email: 'john@gmail.com',
       password: 'cookies',
+      email: 'john@gmail.com',
       entries: 0,
       joined: new Date()
     },
     {
       id: '124',
       name: 'Sally',
-      email: 'sally @gmail.com',
       password: 'bananas',
+      email: 'sally @gmail.com',
       entries: 0,
       joined: new Date()
+    }
+  ],
+  login: [
+    {
+      id: '987',
+      hash: '',
+      email: 'john@gmail.com'
     }
   ]
 }
@@ -31,7 +43,7 @@ app.get('/', (req, res) => {
 
 app.post('/signin', (req,res) => {
   if(req.body.email === database.users[0].email && req.body.password === database.users[0].password){
-    res.json("success");
+    res.json(database.users[0]);
   }
   else{
     res.status(400).json("error logging in")
@@ -39,13 +51,12 @@ app.post('/signin', (req,res) => {
 })
 
 app.post('/register', (req,res) => {
-  const { email, name, password } = req.body; // destructuring the request and assigning the values of it to constant variables
-  var lastUserIndex = database.users.length-1;
+  const { email, name } = req.body; // destructuring the request and assigning the values of it to constant variables
+  let lastUserIndex = database.users.length-1;
   database.users.push({ // pushing a new user object into the user array! NB v useful
     id: '125', // need to make dynamic
       name: name,
       email: email,
-      password: password,
       entries: 0,
       joined: new Date()
   })
@@ -54,6 +65,48 @@ app.post('/register', (req,res) => {
   */
   res.json(database.users[lastUserIndex]);
 })
+
+// V useful function for iterating through an array of objects
+app.get('/profile/:id', (req, res) => {
+  const { id } = req.params; // by usering :id syntax for the url endpoint we can access id in the params
+  let found = false;
+  database.users.forEach(user => {
+    if(user.id === id){
+      found = true;
+      return res.json(user);
+    }
+  })
+  if(!found){
+    res.status(400).json('not found');
+  }
+})
+
+//every time an image is sent, update the number of entries 
+app.put('/image', (req,res) => {
+  const { id } = req.body; 
+  let found = false;
+  database.users.forEach(user => {
+    if(user.id === id){
+      found = true;
+      user.entries++;
+      return res.json(user.entries);
+    }
+  })
+  if(!found){
+    res.status(400).json('not found');
+  }
+})
+
+// Load hash from your password DB.
+// bcrypt.hash(password, null, null, function(err, hash) {
+//   console.log(hash);
+// });
+// bcrypt.compare("bacon", hash, function(err, res) {
+//   // res == true
+// });
+// bcrypt.compare("veggies", hash, function(err, res) {
+//   // res = false
+// });
 
 app.listen(3000, () => { // listen on port 3000 and after listen happens run the function i.e the second parameter
   console.log('app is running on port 3000 ')
